320a4dccd37975ea84a14dca9764d41dfe08a77b
codeprince <anyaohaprince122@gmail.com>
2021-06-25 13:16:24 +0100

    added get method to the singlylinkedlist

--- SinglyLinkedList.js
+++ SinglyLinkedList.js
@@ -66,5 +66,14 @@ class SinglyLinkedList {
     return this;
   }
 
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let current = this.head;
+    for (let i = 1; i <= index; i++) {
+      current = current.next
+    }
+    return current;
+  }
 
 }
+
