59848c99ae06cf93de3d5876de1ab31b343a73e2
codeprince <anyaohaprince122@gmail.com>
2022-03-05 17:07:57 +0100

    added insert method

--- MaxBiinaryHeap.js
+++ MaxBiinaryHeap.js
@@ -1,5 +1,21 @@
 class MaxBinaryHeap {
   constructor() {
-    this.value = [];
+    this.values = [];
+  }
+
+  insert(value) {
+    if (!this.values.length) {
+      this.values.push(value);
+      return this;
+    }
+    const index = this.values.length;
+    const parent = Math.floor((index - 1) / 2);
+    if (this.values[parent] < value) {
+      [this.values[parent], this.values[index]] = [
+        this.values[index],
+        this.values[parent],
+      ];
+    }
+    return this;
   }
 }
