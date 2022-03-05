2c4893c43bd229bf8b84c6f2a1a47bfcf0f01637
codeprince <anyaohaprince122@gmail.com>
2022-03-05 10:00:23 +0100

    added first condition for inserting inn a binary search tree

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -6,4 +6,26 @@ class Node {
   }
 }
 
-class BinarySearchTree {}
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    var newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      var current = this.root;
+      while (true) {
+        if (valeu < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          }
+        }
+      }
+    }
+  }
+}
