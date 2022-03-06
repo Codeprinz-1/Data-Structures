7a59796c9ca8b850f170eeadad6f57fce45f466e
codeprince <anyaohaprince122@gmail.com>
2022-03-06 14:25:53 +0100

    removed log statements

--- MaxBinaryHeap.js
+++ MaxBinaryHeap.js
@@ -57,22 +57,3 @@ class MaxBinaryHeap {
     return max;
   }
 }
-
-const binaryHeap = new MaxBinaryHeap();
-binaryHeap.insert(7);
-binaryHeap.insert(6);
-binaryHeap.insert(3);
-binaryHeap.insert(9);
-binaryHeap.insert(1);
-binaryHeap.insert(34);
-binaryHeap.insert(78);
-binaryHeap.insert(0);
-binaryHeap.insert(34);
-
-const validateHeap = (list) => {
-  for (let i = 0; i < list.length; i++) {
-    const parent = Math.floor((i - 1) / 2);
-    if (list[parent] < list[i]) return false;
-  }
-  return true;
-};
