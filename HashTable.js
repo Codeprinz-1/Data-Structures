8f0d5cc2e40a45f78f26a2ad8746340f6912b2e0
codeprince <anyaohaprince122@gmail.com>
2022-03-07 20:42:04 +0100

    added keymap toconstructor

--- HashTable.js
+++ HashTable.js
@@ -1,16 +1,28 @@
 class HashTable {
-  constructor() {
-    this.size = 53;
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
   }
 
-  _hash() {
+  _hash(key) {
     let total = 0;
     let WEIRD_PRIME = 31;
     for (let i = 0; i < Math.min(key.length, 100); i++) {
       let char = key[i];
       let value = char.charCodeAt(0) - 96;
-      total = (total * WEIRD_PRIME + value) % this.size;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
     }
     return total;
   }
+
+  set(key, value) {
+    const index = this._hash(key);
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+    this.keyMap[index].push(key);
+    return index;
+  }
 }
+
+const ht = new HashTable();
+console.log(ht.set("hello world", "goodbye!!"));
