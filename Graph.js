3c3be3e848bd78fcb174e3eb840db6946fe9a228
codeprince <anyaohaprince122@gmail.com>
2022-03-08 06:39:39 +0100

    added tests

--- Graph.js
+++ Graph.js
@@ -28,4 +28,24 @@ class Graph {
     }
     delete this.adjacencyList[vertex];
   }
+
+  deptFirstTraversal(node) {}
 }
+
+const graph = new Graph();
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
+
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("B", "D");
+graph.addEdge("C", "E");
+graph.addEdge("D", "E");
+graph.addEdge("D", "F");
+graph.addEdge("E", "F");
+
+console.log(graph.adjacencyList);
