7c6bb80b7ca94f0af78a2418a0d4ccccafc1776e
codeprince <anyaohaprince122@gmail.com>
2022-03-08 08:23:27 +0100

    cleaned up graph

--- Graph.js
+++ Graph.js
@@ -90,21 +90,3 @@ class Graph {
     return result;
   }
 }
-
-const graph = new Graph();
-graph.addVertex("A");
-graph.addVertex("B");
-graph.addVertex("C");
-graph.addVertex("D");
-graph.addVertex("E");
-graph.addVertex("F");
-
-graph.addEdge("A", "B");
-graph.addEdge("A", "C");
-graph.addEdge("B", "D");
-graph.addEdge("C", "E");
-graph.addEdge("D", "E");
-graph.addEdge("D", "F");
-graph.addEdge("E", "F");
-console.log(graph.breadthFirstTraverse("A"));
-console.log(graph.adjacencyList);
