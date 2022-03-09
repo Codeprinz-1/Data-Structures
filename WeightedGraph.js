518e81f63816ac6013882954cfa50fd3776def85
codeprince <anyaohaprince122@gmail.com>
2022-03-10 00:18:39 +0100

    added a better priority queue implementation

--- WeightedGraph.js
+++ WeightedGraph.js
@@ -1,19 +1,68 @@
+class Node {
+  constructor(value, priority) {
+    this.value = value;
+    this.priority = priority;
+  }
+}
+
 class PriorityQueue {
   constructor() {
     this.values = [];
   }
 
-  enqueue(val, priority) {
-    this.values.push({ val, priority });
-    this.sort();
+  enqueue(value, priority) {
+    const newNode = new Node(value, priority);
+    this.values.push(newNode);
+    const reshuffle = (index) => {
+      const parent = Math.floor((index - 1) / 2);
+      if (this.values[parent] && this.values[parent].priority < priority) {
+        [this.values[parent], this.values[index]] = [
+          this.values[index],
+          this.values[parent],
+        ];
+        reshuffle(parent);
+      }
+    };
+    reshuffle(this.values.length - 1);
+    return this;
   }
 
-  dequeue() {
-    return this.values.shift();
-  }
+  deQueue() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    const length = this.values.length;
+    const sinkDown = (index = 0) => {
+      const element = this.values[index];
+      const leftChildIndex = 2 * index + 1;
+      const rightChildIndex = 2 * index + 2;
+      let leftChild, rightChild;
+      let swap = null;
 
-  sort() {
-    this.values.sort((a, b) => a.priority - b.priority);
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild.priority > element.priority) {
+          swap = leftChildIndex;
+        }
+      }
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+        if (
+          (swap === null && rightChild.priority > element.priority) ||
+          (swap !== null && rightChild.priority > leftChild.priority)
+        ) {
+          swap = rightChildIndex;
+        }
+      }
+      if (swap === null) return;
+      this.values[index] = this.values[swap];
+      this.values[swap] = element;
+      sinkDown(swap);
+    };
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      sinkDown();
+    }
+    return max;
   }
 }
 
